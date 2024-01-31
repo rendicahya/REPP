dccfb18b562fa68da3905bb78cac0276cee2a34e
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-01-31 13:08:24 +0900

    Added export to .npz masks

--- batch.py
+++ batch.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 import warnings
 from pathlib import Path
 
@@ -22,17 +23,18 @@ n_files = count_files(pckl_in_dir, ext=".pckl")
 repp_params = json.load(open(repp_conf, "r"))
 repp = REPP(**repp_params, store_coco=True)
 generate_video = conf.repp.output.video.generate
+bundle_mask = conf.repp.output.mask.bundle
+video_in_ext = conf.repp.input.video.ext
+video_in_root = project_root / conf.repp.input.video.path
 
 assert_that(pckl_in_dir).is_directory().is_readable()
 assert_that(repp_conf).is_file().is_readable()
 
 if generate_video:
-    video_root = project_root / conf.repp.input.video.path
-    video_in_ext = conf.repp.input.video.ext
     video_out_root = project_root / conf.repp.output.video.path
     video_out_ext = conf.repp.output.video.ext
 
-    assert_that(video_root).is_directory().is_readable()
+    assert_that(video_in_root).is_directory().is_readable()
     assert_that(video_out_ext).is_type_of(str).matches(r"^\.[a-zA-Z0-9]{3}$")
 
 warnings.filterwarnings("ignore")
@@ -45,46 +47,69 @@ for pckl in pckl_in_dir.glob("**/*.pckl"):
     action = pckl.parent.name
     total_preds = []
 
+    video_name = pckl.with_suffix(video_in_ext).name
+    video_path = video_in_root / action / video_name
+
+    if not video_path.exists():
+        print("Video not found:", video_path)
+        continue
+
+    vid_info = video_info(video_path)
+    vid_width, vid_height = vid_info["height"], vid_info["width"]
+    n_frames = vid_info["n_frames"]
+
+    if bundle_mask:
+        mask_bundle = np.zeros((n_frames, vid_height, vid_width), np.uint8)
+        mask_out_path = mask_out_dir / action / pckl.stem
+
+    if generate_video:
+        frames = video_frames(video_path, reader=conf.repp.input.video.reader)
+        out_frames = []
+
     for vid, video_preds in get_video_frame_iterator(pckl):
         preds_coco, _ = repp(video_preds)
         total_preds += preds_coco
 
-    if generate_video:
-        video_name = pckl.with_suffix(video_in_ext).name
-        video_path = video_root / action / video_name
+    for f in range(n_frames):
+        boxes = [item["bbox"] for item in total_preds if int(item["image_id"]) == f]
 
-        assert_that(video_path).is_file().is_readable()
-
-        vid_info = video_info(video_path)
-        video_width, video_height = vid_info["height"], vid_info["width"]
-        frames = video_frames(video_path, reader=conf.repp.input.video.reader)
-        output_frames = []
-
-        for i, frame in enumerate(frames):
-            boxes = [item["bbox"] for item in total_preds if int(item["image_id"]) == i]
-            mask = np.zeros((video_width, video_height), np.uint8)
-
-            for box in boxes:
-                x1, y1, w, h = [round(v) for v in box]
-                x2, y2 = x1 + w, y1 + h
-                mask[y1:y2, x1:x2] = 255
-
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-
+        if not bundle_mask:
+            mask = np.zeros((vid_width, vid_height), np.uint8)
             mask_out_path = mask_out_dir / action / pckl.stem / f"{i:05}.png"
 
-            mask_out_path.parent.mkdir(exist_ok=True, parents=True)
-            output_frames.append(frame)
+        if generate_video:
+            frame = next(frames)
+
+        for box in boxes:
+            x1, y1, w, h = [round(v) for v in box]
+            x2, y2 = x1 + w, y1 + h
+
+            if bundle_mask:
+                mask_bundle[f, y1:y2, x1:x2] = 255
+            else:
+                mask[y1:y2, x1:x2] = 255
+
+            if generate_video:
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+        mask_out_path.parent.mkdir(exist_ok=True, parents=True)
+
+        if generate_video:
+            out_frames.append(frame)
+
+        if not bundle_mask:
             cv2.imwrite(str(mask_out_path), mask)
 
-        output_video_path = (
-            video_out_root / action / pckl.with_suffix(video_out_ext).name
-        )
+    if bundle_mask:
+        np.savez_compressed(mask_out_path, mask_bundle)
 
-        output_video_path.parent.mkdir(parents=True, exist_ok=True)
+    if generate_video:
+        video_out_path = video_out_root / action / pckl.with_suffix(video_out_ext).name
+
+        video_out_path.parent.mkdir(parents=True, exist_ok=True)
         frames_to_video(
-            frames=output_frames,
-            target=output_video_path,
+            frames=out_frames,
+            target=video_out_path,
             writer=conf.repp.output.video.writer,
             fps=vid_info["fps"],
         )
